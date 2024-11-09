353de237ede3d30a3fda241478a096f6d6e6a20e
HyperWinX <artmsmth@gmail.com>
2024-11-09 15:29:55 +0000

    Implemented comparison functions

--- include/ctestify.h
+++ include/ctestify.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 // Defining internal types
 typedef void(*test)(void);
 typedef struct _test {
@@ -10,8 +12,25 @@ typedef struct _test {
   char* line;
 } _test;
 
+typedef enum ComparisonResult {
+  BIGGER,
+  LESS,
+  EQ,
+  NOT_EQ
+} ComparisonResult;
+
 extern void __ctestify_register_ctest(struct _test* _test_ptr);
 extern void __ctestify_run_all_tests();
+
+extern ComparisonResult __ctestify_compare_uint8_t(uint8_t, uint8_t);
+extern ComparisonResult __ctestify_compare_uint16_t(uint16_t, uint16_t);
+extern ComparisonResult __ctestify_compare_uint32_t(uint32_t, uint32_t);
+extern ComparisonResult __ctestify_compare_uint64_t(uint64_t, uint64_t);
+extern ComparisonResult __ctestify_compare_int8_t(int8_t, int8_t);
+extern ComparisonResult __ctestify_compare_int16_t(int16_t, int16_t);
+extern ComparisonResult __ctestify_compare_int32_t(int32_t, int32_t);
+extern ComparisonResult __ctestify_compare_int64_t(int64_t, int64_t);
+extern ComparisonResult __ctestify_compare_string(char*, char*);
 
 #define _TEST_FUNC(suite, test_name) __ctest_##suite_##test_name
 #define _TEST_PROP(suite, test_name) __test_repr_##suite_##test_name
@@ -26,3 +45,9 @@ extern void __ctestify_run_all_tests();
   void _TEST_FUNC(suite, test_name)(void)
 
 #define RUN_ALL_TESTS() __ctestify_run_all_tests()
+
+#define __CTESTIFY_COMPARE(_val, _correct_val) _Generic((_val), \
+  uint8_t: )
+
+#define EXPECT_EQ(val, correct)
+#define EXPECT_EQ(val, correct, err_msg)
--- src/ctestify_impl.c
+++ src/ctestify_impl.c
@@ -1,6 +1,10 @@
-#include <asm-generic/fcntl.h>
+#include <fcntl.h>
+#include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
 //Colors ANSI escape sequences
 #define CRED     "\x1b[31m"
@@ -13,10 +17,19 @@
 typedef void(*test)(void);
 typedef struct _test {
   struct _test* next;
-                                                    test test_ptr;                                    char* suite_name;                                 char* name;
+  test test_ptr;
+  char* suite_name;
+  char* name;
   char* file;
   char* line;
 } _test;
+
+typedef enum ComparisonResult {
+  BIGGER,
+  LESS,
+  EQ,
+  NOT_EQ
+} ComparisonResult;
 
 //Internal data
 HIDDEN _test* head = NULL;
@@ -31,7 +44,55 @@ HIDDEN long double __calc_test_time() {
   return ((long double)(ct_tend - ct_tstart)) / CLOCKS_PER_SEC;
 }
 
+// Comparison functions
+ComparisonResult __ctestify_compare_uint8_t(uint8_t a, uint8_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_uint16_t(uint16_t a, uint16_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_uint32_t(uint32_t a, uint32_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_uint64_t(uint64_t a, uint64_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_int8_t(int8_t a, int8_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_int16_t(int16_t a, int16_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_int32_t(int32_t a, int32_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_int64_t(int64_t a, int64_t b) {
+  if (a < b) return LESS;
+  else if (a > b) return BIGGER;
+  else return EQ;
+}
+ComparisonResult __ctestify_compare_string(char* a, char* b) {
+  if (strcmp(a, b)) return NOT_EQ;
+  else return EQ;
+}
+
+
 void __ctestify_verify_result(int result) {
+  long double time = __calc_test_time();
   if (result) {
     fprintf(ct_stdout, "%s%s%s %s.%s (%.3Lf%s)\n", CGREEN, "[      OK ]", CRESET, current->suite_name, current->name, time < 1000 ? time * 1000 : time, time < 1000 ? "ms" : "s"); \
   } else {
